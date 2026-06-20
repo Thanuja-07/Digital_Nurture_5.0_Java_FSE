6e893217a999125ff2276fdc420a1562a5407e98
Thanuja <thanujasasi145@gmail.com>
2026-06-20 22:15:45 +0530

    Add files via upload

---
+++ SingletonPattern.java
@@ -0,0 +1,51 @@
+class Logger{
+    private static Logger instance = null;
+    private Logger() {
+        System.out.println("Logger instance created");
+    }
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First log");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second log");
+
+        System.out.println("Are both logger instances the same? " + (logger1 == logger2));
+    }
+}
+
+/*class Logger {
+
+    public Logger() {
+        System.out.println("Logger instance created");
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = new Logger();
+        logger1.log("First log");
+
+        Logger logger2 = new Logger();
+        logger2.log("Second log");
+
+        System.out.println("Are both logger instances the same? " + (logger1 == logger2));
+    }
+}*/
